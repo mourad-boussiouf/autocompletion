9c1e302108c90dd6c90e172b60598620163b8bba
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-05-11 15:48:15 +0200

    déclaration des query selectors

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-
-
+var searchinputs =  document.querySelectorAll("input");
+var spancomplete1 = document.getElementById('search_result1');
+var spancomplete2 = document.getElementById('search_result2');
 
 
 
