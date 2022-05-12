aba9c6f210501d817fbdc9491ea71633645169e9
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-05-13 00:28:46 +0200

    écriture de la function de recherche js côté client dans le json renvoyé par traitement.php

--- js/script.js
+++ js/script.js
@@ -6,11 +6,16 @@ const searchinputs2 = document.getElementById('searchbox2');
 const spancomplete1 = document.getElementById('search_result1');
 const spancomplete2 = document.getElementById('search_result2');
 
-searchinputs1.addEventListener('input', searchinputs1);
+
+// search the json $result 
+
+const searchSaiyan = async searchAll =>
+
+searchinputs1.addEventListener('input', () => searchSaiyan(searchinputs1.value));
 
 
 
-searchinputs2.addEventListener('input', searchinputs2);
+searchinputs2.addEventListener('input', () => searchSaiyan(searchinputs2.value));
 
 
 })
