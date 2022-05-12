d8f07fcb83b60700632999fbd5f26136327aa703
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-05-13 00:09:51 +0200

    addeventlisten d'input sur les deux champs

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-var searchinputs1 =  document.getElementById('searchbox1');
-var searchinputs2 = document.getElementById('searchbox2');
-var spancomplete1 = document.getElementById('search_result1');
-var spancomplete2 = document.getElementById('search_result2');
 
+
+const searchinputs1 =  document.getElementById('searchbox1');
+const searchinputs2 = document.getElementById('searchbox2');
+const spancomplete1 = document.getElementById('search_result1');
+const spancomplete2 = document.getElementById('search_result2');
+
+searchinputs1.addEventListener('input', searchinputs1);
+
+
+
+searchinputs2.addEventListener('input', searchinputs2);
 
 
 })
+
+
+
+
+
+
+
+
