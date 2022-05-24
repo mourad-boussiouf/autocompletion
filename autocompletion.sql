2f3b6a5cda827e77120d94c05f9d844adca4e232
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-05-24 03:38:09 +0200

    maj fichier .sql

--- autocompletion.sql
+++ autocompletion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 12 mai 2022 à 17:48
+-- Généré le : mar. 24 mai 2022 à 01:37
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.4.9
 
@@ -32,31 +32,36 @@ CREATE TABLE IF NOT EXISTS `personnages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `personnages`
 --
 
 INSERT INTO `personnages` (`id`, `nom`) VALUES
-(1, 'Son Goku'),
+(1, 'Goku'),
 (2, 'Bulma'),
 (3, 'Krillin'),
 (4, 'Piccolo'),
-(5, 'Son Gohan'),
+(5, 'Gohan'),
 (6, 'Vegeta'),
 (7, 'Bardock'),
 (8, 'Trunks'),
 (9, 'Yamcha'),
-(10, 'Android 18'),
+(10, 'C-18'),
 (11, 'Nappa'),
 (12, 'Raditz'),
 (13, 'Cell'),
 (14, 'Broly'),
 (15, 'Yajirobe'),
 (16, 'Videl'),
+(21, 'Chichi'),
+(22, 'Ginue'),
+(23, 'Chaozu'),
 (17, 'Buu'),
-(18, 'Freezer');
+(18, 'Freezer'),
+(19, 'Goten'),
+(20, 'Tenshinhan');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
